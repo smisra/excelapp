76e247fb086051616339a67fb6b08d723f40d3a8
smisra <misrasandeep@yahoo.com>
2019-07-24 22:07:54 +0100

    encryption

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,39 @@ var express = require('express');
 var router = express.Router();
 const fs = require("fs");
 const Excel = require('exceljs');
+const { Safe } = require("../safe");
 
 /* GET Excel. */
+router.get('/encrypt/:guid', function(req, res, next) {
+  console.log('indide enc');
+  guid_p = req.params.guid;
+  if(guid_p==""){console.log("No GUID passed");
+  res.render('error', { title: 'No Guid' });
+}
+  var safe = new Safe(__dirname+"/../data/"+guid_p+"/data.json", "password");
+  var data1 = [
+    [ 'medicine 1', 10, 1.10],
+    [ 'medicine 2', 30, 0.40],
+    [ 'ointment 1', 15, 0.45],
+    [ 'ointment 2', 20, 0.49],
+];
+  
+    var data  = safe.encrypt(data1);
+    res.set('Content-Type', 'text/html');
+    res.send(Buffer.from('<h2>Encrypted</h2>'));
+  res.end();
+  
+});
 router.get('/:guid', function(req, res, next) {
   guid_p = req.params.guid;
   if(guid_p==""){console.log("No GUID passed");
   res.render('error', { title: 'No Guid' });
 }
-  if (fs.existsSync(__dirname+'/../upload/'+guid_p)) {
+  if (fs.existsSync(__dirname+'/../data/'+guid_p)) {
     console.log('The file exists.');
-    res.render('index', { title: 'Express',guid: guid_p });
+    var safe = new Safe(__dirname+"/../data/"+guid_p+"/data.json", "password");
+    var data  = safe.decrypt();
+    res.render('index', { title: 'Sainsburys',guid: guid_p,data: data});
   }
   else{
     res.render('error', { title: 'No Guid',message: 'wrong link' });
@@ -34,6 +57,7 @@ router.post('/:guid', function(req, res, next) {
   }
   
 });
+
 async function createExcel(contents,guid){
   console.log(contents);
   const workbook = new Excel.Workbook();
